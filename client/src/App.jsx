f5185e36a50f8c0d5334bc4dc6a87f3905c453b9
Brian <omogabrian2060@outlook.com>
2021-09-16 07:37:44 +0300

    set up the client section and insalled react

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+import styled from "styled-components"
+
+const Container = styled.div`
+  
+`
+
+const App = () => {
+  return <div>
+    <Home />
+  </div>;
+};
+
+export default App;
